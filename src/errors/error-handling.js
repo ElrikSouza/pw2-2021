9a416f08d04ffabab40e2a1857c3773139d678ad
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-09-19 16:56:52 -0400

    created a middleware wrapper for handling exceptions

--- src/errors/error-handling.js
+++ src/errors/error-handling.js
@@ -6,3 +6,13 @@ export const wrapWithErrorHandling = (handler) => async (req, res, next) => {
     next(error);
   }
 };
+
+export const wrapMiddlewareWithErrorHandling =
+  (handler) => async (req, res, next) => {
+    try {
+      const result = await handler(req, res, next);
+      return result;
+    } catch (error) {
+      next(error);
+    }
+  };
